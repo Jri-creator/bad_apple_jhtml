aaad66c0f4c85ce42accf78f74774b8e49d4d9af
Jri-creator <145695642+Jri-creator@users.noreply.github.com>
2024-11-04 23:50:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ async function displayTextFile(index) {
     try {
         const response = await fetch(`txt/${index}.txt`);
         if (!response.ok) {
-            throw new Error('Text file not found');
+            throw new Error('Text file wasn't arrayed.');
         }
         const text = await response.text();
         textDisplay.textContent = text; // Preserve formatting by using textContent
     } catch (error) {
         console.error('Error:', error);
-        textDisplay.textContent = 'Error loading text file.';
-    }
+        textDisplay.textContent = 'Error loading text file. Check your network connection. Once restored, the animation will return.';
+    } 
 }
 
 // Function to update the text display at regular intervals
